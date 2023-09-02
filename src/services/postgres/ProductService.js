c4cfa096ad580dab45b306e31657e9d46bdf3b4f
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2023-09-02 22:14:32 +0800

    add get, edit and delete Product

--- src/services/postgres/ProductService.js
+++ src/services/postgres/ProductService.js
@@ -1,6 +1,8 @@
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModelProduct } = require('../../utils/model');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class ProductService {
   constructor() {
@@ -15,7 +17,7 @@ class ProductService {
     const updatedAt = createdAt;
 
     const query = {
-      text: 'INSERT INTO product VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+      text: 'INSERT INTO product VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, created_by',
       values: [id, name, category, price, createdAt, updatedAt, createdBy],
     };
 
@@ -25,6 +27,49 @@ class ProductService {
     }
 
     return result.rows[0].id;
+  }
+
+  async getAllProducts() {
+    const result = await this._pool.query('SELECT * FROM product');
+    return result.rows.map(mapDBToModelProduct);
+  }
+
+  async getProductById(id) {
+    const query = {
+      text: 'SELECT * FROM product WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Produk tidak ditemukan');
+    }
+    return result.rows.map(mapDBToModelProduct)[0];
+  }
+
+  async editProductById(id, { name, category, price }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE product SET name = $1, category = $2, price = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [name, category, price, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Gagal memperbarui Produk. id tidak ditemukan');
+    }
+  }
+
+  async deleteProductById(id) {
+    const query = {
+      text: 'DELETE FROM product WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Produk gagal dihapus. id tidak ditemukan');
+    }
   }
 }
 
