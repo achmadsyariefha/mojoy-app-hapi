ab364b1adde7c59729c7141d5edbd583ad0dac49
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2023-09-13 22:53:04 +0800

    Update UsersService.js

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,8 +1,14 @@
 const { Pool } = require('pg');
+const { mapDBToModelUser } = require('../../utils/model');
 
 class UsersService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async getAllUsers() {
+    const result = await this._pool.query('SELECT * FROM users');
+    return result.rows.map(mapDBToModelUser);
   }
 }
 
