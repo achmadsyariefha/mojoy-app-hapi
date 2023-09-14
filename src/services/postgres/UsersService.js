c961151fc7253d82ddfb967c79c2679d64bffebc
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2023-09-14 23:17:25 +0800

    Update UsersService.js

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,9 +1,27 @@
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
 const { mapDBToModelUser } = require('../../utils/model');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class UsersService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addUser({ username, email, password }) {
+    const id = `user-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, email, password],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Gagal menambahkan pengguna');
+    }
+
+    return result.rows[0].id;
   }
 
   async getAllUsers() {
